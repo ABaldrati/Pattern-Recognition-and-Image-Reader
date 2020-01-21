f26b9cc4a621bfb43012f3e5ddc2e38d8246c326
Alberto <a_baldrati@hotmail.it>
2020-01-21 21:13:26 +0100

    feat(Sequence): change seed of generator

--- Sequence.cpp
+++ Sequence.cpp
@@ -1,11 +1,13 @@
 //
 // Created by alberto on 21/01/20.
 //
-
+#include <ctime>
 #include "Sequence.h"
 #include <random>
+#include <iostream>
+
 void Sequence::generateRandomUniformSequence() {
-    std::default_random_engine generator;
+    std::default_random_engine generator(time(NULL));
     std::uniform_int_distribution<int> distribution(-max_value,max_value);
     for(int i=0;i<length;i++){
         values[i]=distribution(generator);
