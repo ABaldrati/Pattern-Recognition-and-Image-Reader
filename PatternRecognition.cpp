a158f98c4e6905dc4906006549712848f1ff9381
Alberto <a_baldrati@hotmail.it>
2020-01-23 16:04:26 +0100

    feat(PatternRecognition): add openMP features

--- PatternRecognition.cpp
+++ PatternRecognition.cpp
@@ -6,26 +6,42 @@
 #include <iostream>
 #include "PatternRecognition.h"
 
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
 void PatternRecognition::findPattern() {
-    int* sads = new int[target_sequence.getLength()-query_sequence.getLength()+1];
-    for(int i=0;i<target_sequence.getLength()-query_sequence.getLength()+1;i++){
-        sads[i]=0;
-        for(int j=0;j<query_sequence.getLength();j++){
-            sads[i]+=std::abs(target_sequence.getValues()[i+j]-query_sequence.getValues()[j]);
+    int sads_length = target_sequence.getLength() - query_sequence.getLength() + 1;
+    int *sads = new int[sads_length];
+#pragma omp parallel for schedule(static) shared(sads, sads_length) default(none)
+    for (int i = 0; i < sads_length; i++) {
+        sads[i] = 0;
+        for (int j = 0; j < query_sequence.getLength(); j++) {
+            std::cout << "target = " << target_sequence.getValues() << std::endl;
+            int i1 = target_sequence.getValues()[i + j];
+            int i2 = query_sequence.getValues()[j];
+
+            sads[i] += std::abs(i1 - i2);
         }
     }
 
-    begin = min_element_array(sads,target_sequence.getLength()-query_sequence.getLength()+1);
+    std::cout << std::endl << "sads ";
+    for (int i = 0; i < target_sequence.getLength() - query_sequence.getLength() + 1; i++) {
+        std::cout << sads[i] << " ";
+    }
+    std::cout << std::endl;
+
+    begin = min_element_array(sads, sads_length);
 }
 
 void PatternRecognition::PrintPattern() {
-    for (int i=begin;i<begin + query_sequence.getLength();i++){
+    for (int i = begin; i < begin + query_sequence.getLength(); i++) {
         std::cout << target_sequence.getValues()[i] << " ";
     }
 
     std::cout << std::endl;
 
-    for (int i=0;i<query_sequence.getLength();i++){
+    for (int i = 0; i < query_sequence.getLength(); i++) {
         std::cout << query_sequence.getValues()[i] << " ";
     }
 
