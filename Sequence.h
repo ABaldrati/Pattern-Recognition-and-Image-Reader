299da62bce9c6999b895924bd5a8085a4bb40569
Alberto <a_baldrati@hotmail.it>
2020-01-25 16:16:35 +0100

    feat(Sequence): remove length using vector

--- Sequence.h
+++ Sequence.h
@@ -8,7 +8,6 @@
 #include "Utils.h"
 #include <vector>
 #include <ctime>
-#include "Sequence.h"
 #include <random>
 #include <iostream>
 
@@ -17,34 +16,26 @@ class Sequence {
 
 public:
 
-    Sequence(const int length) : length(length) {
-        values = std::vector<int>(length);
+    explicit Sequence(const int length) {
+        values = std::vector<T>(length);
     }
-
 
     void generateRandomUniformSequence(const T max_value) {
         std::random_device r;
         std::default_random_engine generator(r());
-        std::uniform_int_distribution<T> distribution(-max_value, max_value);
-        for (int i = 0; i < length; i++) {
+        std::uniform_real_distribution<T> distribution(-max_value, max_value);
+        for (int i = 0; i < values.size(); i++) {
             values[i] = distribution(generator);
         }
     }
 
-
-    int getLength() const {
-        return length;
-    }
-
-    const std::vector<int> &getValues() const {
+    const std::vector<T> &getValues() const {
         return values;
     }
 
 private:
-    const int length;
-    std::vector<int> values;
+    std::vector<T> values;
 
 };
-
 
 #endif //PATTERN_RECOGNITION_SEQUENCE_H
