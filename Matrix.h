3612239f4e7886d4848775574bacc0327e46f68a
Alberto <a_baldrati@hotmail.it>
2020-01-25 16:12:31 +0100

    feat: remove length from everywhere

---
+++ Matrix.h
@@ -0,0 +1,13 @@
+//
+// Created by alberto on 25/01/20.
+//
+
+#ifndef PATTERN_RECOGNITION_MATRIX_H
+#define PATTERN_RECOGNITION_MATRIX_H
+
+template <typename T>
+class Matrix{
+
+};
+
+#endif //PATTERN_RECOGNITION_MATRIX_H
--- PatternRecognition.h
+++ PatternRecognition.h
@@ -9,7 +9,6 @@
 #include "Utils.h"
 #include <cmath>
 #include <iostream>
-#include "PatternRecognition.h"
 
 #ifdef _OPENMP
 #include <omp.h>
@@ -20,28 +19,26 @@ class PatternRecognition {
 public:
 
     PatternRecognition(const Sequence<T> &query_sequence, const Sequence<T> &target_sequence) : query_sequence(
-            query_sequence),
-                                                                                                target_sequence(
-                                                                                                        target_sequence) {
+            query_sequence), target_sequence(target_sequence) {
         begin = 0;
     }
 
     void findPattern() {
-        int sads_length = target_sequence.getLength() - query_sequence.getLength() + 1;
-        int *sads = new int[sads_length];
+        int sads_length = target_sequence.getValues().size() - query_sequence.getValues().size() + 1;
+        T *sads = new T[sads_length];
 #pragma omp parallel for schedule(static) shared(sads, sads_length) default(none)
         for (int i = 0; i < sads_length; i++) {
             sads[i] = 0;
-            for (int j = 0; j < query_sequence.getLength(); j++) {
-                int target_value = target_sequence.getValues()[i + j];
-                int query_value = query_sequence.getValues()[j];
+            for (int j = 0; j < query_sequence.getValues().size(); j++) {
+                T target_value = target_sequence.getValues()[i + j];
+                T query_value = query_sequence.getValues()[j];
 
                 sads[i] += std::abs(target_value - query_value);
             }
         }
 
         std::cout << std::endl << "sads ";
-        for (int i = 0; i < target_sequence.getLength() - query_sequence.getLength() + 1; i++) {
+        for (int i = 0; i < sads_length; i++) {
             std::cout << sads[i] << " ";
         }
         std::cout << std::endl;
@@ -49,14 +46,14 @@ public:
         begin = min_element_array(sads, sads_length);
     }
 
-    void PrintPattern(){
-        for (int i = begin; i < begin + query_sequence.getLength(); i++) {
+    void PrintPattern() {
+        for (int i = begin; i < begin + query_sequence.getValues().size(); i++) {
             std::cout << target_sequence.getValues()[i] << " ";
         }
 
         std::cout << std::endl;
 
-        for (int i = 0; i < query_sequence.getLength(); i++) {
+        for (int i = 0; i < query_sequence.getValues().size(); i++) {
             std::cout << query_sequence.getValues()[i] << " ";
         }
     }
