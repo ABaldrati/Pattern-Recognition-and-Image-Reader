fce7acc7dbe16fc13c8104a5bb899d9e6cfff3e8
Alberto <a_baldrati@hotmail.it>
2020-01-27 12:37:02 +0100

    feat(matrix): add matrix functionalities

--- Matrix.h
+++ Matrix.h
@@ -5,8 +5,70 @@
 #ifndef PATTERN_RECOGNITION_MATRIX_H
 #define PATTERN_RECOGNITION_MATRIX_H
 
-template <typename T>
-class Matrix{
+#include <vector>
+#include <iostream>
+#include <random>
+
+template<typename T>
+class Matrix {
+public:
+
+    Matrix(int r, int c) : rows(r), columns(c) {
+        if (r < 1)
+            rows = 1;
+        if (c < 1)
+            columns = 1;
+        values = std::vector<T>(rows * columns);
+    }
+
+    Matrix(const Matrix &rh) {
+        rows = rh.rows;
+        columns = rh.columns;
+        values = std::vector<T>(rows * columns);
+        for (int i = 0; i < rows * columns; i++)
+            values[i] = rh.values[i];
+    }
+
+    void generateRandomUniformMatrix(const T max_value) {
+        std::random_device r;
+        std::default_random_engine generator(r());
+        std::uniform_real_distribution<T> distribution(-max_value, max_value);
+        for (int i = 0; i < rows * columns; i++) {
+            values[i] = distribution(generator);
+        }
+    }
+
+    int getNumberOfRows() const {
+        return rows;
+    }
+
+    int getNumberOfColumns() const {
+        return columns;
+    }
+
+    const std::vector<T> &getValues() const {
+        return values;
+    }
+
+    T getValue(int i, int j) const {
+        if (i >= rows || i < 0 || j < 0 || j >= columns) {
+            throw std::out_of_range("Elemento non presente nella matrice read");
+        }
+        return values[columns * i + j];
+    }
+
+    void setValue(int i, int j, const T &value) {
+        if (i >= rows || j >= columns) {
+            throw std::out_of_range("Elemento non presente nella matrice write");
+        }
+        values[columns * i + j] = value;
+    }
+
+
+private:
+    std::vector<T> values;
+    int rows;
+    int columns;
 
 };
 
