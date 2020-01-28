859424ea2a61f4169c0bec9fa167d3d26d4950d2
Alberto <a_baldrati@hotmail.it>
2020-01-28 11:01:32 +0100

    feat(Sequence): add functionalities to manage both float and int

--- Sequence.h
+++ Sequence.h
@@ -24,10 +24,23 @@ public:
         int size = rh.getValues().size();
         values = std::vector<T>(size);
         for (int i = 0; i < size; i++)
-            values[i] = values.buffer[i];
+            values[i] = rh.values[i];
     }
 
     void generateRandomUniformSequence(const T max_value) {
+        return _generateRandomUniformSequence(max_value, std::is_floating_point<T>());
+    }
+
+    void _generateRandomUniformSequence(const T max_value, std::false_type) {
+        std::random_device r;
+        std::default_random_engine generator(r());
+        std::uniform_int_distribution<T> distribution(-max_value, max_value);
+        for (int i = 0; i < values.size(); i++) {
+            values[i] = distribution(generator);
+        }
+    }
+
+    void _generateRandomUniformSequence(const T max_value, std::true_type) {
         std::random_device r;
         std::default_random_engine generator(r());
         std::uniform_real_distribution<T> distribution(-max_value, max_value);
@@ -35,7 +48,6 @@ public:
             values[i] = distribution(generator);
         }
     }
-
 
     const std::vector<T> &getValues() const {
         return values;
