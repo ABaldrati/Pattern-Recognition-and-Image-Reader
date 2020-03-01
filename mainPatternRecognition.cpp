56582c4847a34fe196d62f2e835952dcc43a63f9
Alberto <a_baldrati@hotmail.it>
2020-03-01 12:39:28 +0100

    feat(main pattern recognition): add test code

--- mainPatternRecognition.cpp
+++ mainPatternRecognition.cpp
@@ -7,19 +7,77 @@
 
 
 int main() {
-    auto t1 = std::chrono::high_resolution_clock::now();
-    Matrix<int> target(1500,1500);
-    Matrix<int> query(100, 100);
+    int trow = 1500;
+    int tcol = 1500;
+    int qrow = 150;
+    int qcol = 150;
+    for (int numThread = 1; numThread < 7; ++numThread) {
+        auto t1 = std::chrono::high_resolution_clock::now();
+        Matrix<int> target(trow, tcol);
+        Matrix<int> query(qrow, qcol);
 
-    target.generateRandomUniformMatrix(25);
-    query.generateRandomUniformMatrix(25);
+        target.generateRandomUniformMatrix(10);
+        query.generateRandomUniformMatrix(10);
+        for (int i = 0; i < 5; ++i) {
+            PatternRecognition<int> pattern(query, target);
+            pattern.findPattern(numThread);
+        }
 
-    PatternRecognition<int> pattern(query, target);
-    pattern.findPattern();
+        auto t2 = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
 
-    auto t2 = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+        std::cout << std::endl << "Targetdim " << trow << "x" << tcol << "\tquerydim " << qrow << "x" << qcol
+                  << "\twith numThread " << numThread << "  time:" << float(duration / 5000000.);
+    }
 
-    std::cout << std::endl << float(duration / 1000000.);
+    std::cout << std::endl << std::endl;
 
+    trow = 2000;
+    tcol = 2000;
+    qrow = 200;
+    qcol = 200;
+    for (int numThread = 1; numThread < 7; ++numThread) {
+        auto t1 = std::chrono::high_resolution_clock::now();
+        Matrix<int> target(trow, tcol);
+        Matrix<int> query(qrow, qcol);
+
+        target.generateRandomUniformMatrix(10);
+        query.generateRandomUniformMatrix(10);
+        for (int i = 0; i < 5; ++i) {
+            PatternRecognition<int> pattern(query, target);
+            pattern.findPattern(numThread);
+        }
+
+        auto t2 = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+
+        std::cout << std::endl << "Targetdim " << trow << "x" << tcol << "\tquerydim " << qrow << "x" << qcol
+                  << "\twith numThread " << numThread << "  time:" << float(duration / 5000000.);
+    }
+
+    std::cout << std::endl << std::endl;
+
+
+    trow = 2500;
+    tcol = 2500;
+    qrow = 250;
+    qcol = 250;
+    for (int numThread = 1; numThread < 7; ++numThread) {
+        auto t1 = std::chrono::high_resolution_clock::now();
+        Matrix<int> target(trow, tcol);
+        Matrix<int> query(qrow, qcol);
+
+        target.generateRandomUniformMatrix(10);
+        query.generateRandomUniformMatrix(10);
+        for (int i = 0; i < 5; ++i) {
+            PatternRecognition<int> pattern(query, target);
+            pattern.findPattern(numThread);
+        }
+
+        auto t2 = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+
+        std::cout << std::endl << "Targetdim " << trow << "x" << tcol << "\tquerydim " << qrow << "x" << qcol
+                  << "\twith numThread " << numThread << "  time:" << float(duration / 5000000.);
+    }
 }
