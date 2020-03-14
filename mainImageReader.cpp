9e58ccbd78e5ab0168cbe268de7d6a19db4524ab
ABaldrati <a_baldrati@hotmail.it>
2020-03-15 00:57:08 +0100

    feat(mainimagereader): add test code

--- mainImageReader.cpp
+++ mainImageReader.cpp
@@ -1,37 +1,45 @@
 #include "ImageReader.h"
 
 int main() {
-    int numThreads = 4;
-    boost::basic_thread_pool ThreadPool(numThreads);
+    std::string inputDir = "../test1";
 
-    std::string inputDir = "../input_images";
-    auto t1 = std::chrono::high_resolution_clock::now();
-    std::vector<std::pair<cv::Mat, fs::path>> imagesSeq = sequentialRead(inputDir);
-    auto t2 = std::chrono::high_resolution_clock::now();
+    sleep(15);
 
-    auto t3 = std::chrono::high_resolution_clock::now();
-    std::vector<std::pair<cv::Mat, fs::path>> imagesPar = parallelSyncRead(inputDir, numThreads);
-    auto t4 = std::chrono::high_resolution_clock::now();
-
-    auto t5 = std::chrono::high_resolution_clock::now();
-    std::vector<std::pair<boost::future<cv::Mat>,fs::path>> fut_images1 = asyncParallel(inputDir, ThreadPool);
-    auto imagesAsync1 = std::vector<cv::Mat>();
-
-    for (auto &fut_image : fut_images1) {
-        imagesAsync1.push_back(std::get<0>(fut_image).get());
+    for (int numThreads = 1; numThreads < 11; ++numThreads) {
+        auto t1 = std::chrono::high_resolution_clock::now();
+        for (int i = 0; i < 5; i++) {
+            std::vector<std::pair<cv::Mat, fs::path>> imagesPar = parallelSyncRead(inputDir, numThreads);
+        }
+        auto t2 = std::chrono::high_resolution_clock::now();
+        sleep(5);
+        auto durationSync = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+        std::cout << "Parallel sync read time with numThread " << numThreads << "   " << float(durationSync / 5000000.)
+                  << std::endl;
+        sleep(1);
     }
-    auto t6 = std::chrono::high_resolution_clock::now();
 
+    std::cout << std::endl << std::endl;
 
-    auto durationSeq = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-    auto durationSync = std::chrono::duration_cast<std::chrono::microseconds>(t4 - t3).count();
-    auto durationAsyc = std::chrono::duration_cast<std::chrono::microseconds>(t6 - t5).count();
+    for (int numThreads = 1; numThreads < 11; ++numThreads) {
+        boost::basic_thread_pool ThreadPool(numThreads);
+        sleep(5);
 
+        auto t3 = std::chrono::high_resolution_clock::now();
+        for (int i = 0; i < 5; i++) {
+            std::vector<std::pair<boost::future<cv::Mat>, fs::path>> fut_images1 = asyncParallel(inputDir, ThreadPool);
+            auto imagesAsync1 = std::vector<cv::Mat>();
 
-    std::cout << "Sequential read time: " << float(durationSeq / 1000000.) << std::endl;
-    std::cout << "Parallel sync read time: " << float(durationSync / 1000000.) << std::endl;
-    std::cout << "Parallel async2 read time: " << float(durationAsyc / 1000000.) << std::endl;
+            for (auto &fut_image : fut_images1) {
+                imagesAsync1.push_back(std::get<0>(fut_image).get());
+            }
+        }
+        auto t4 = std::chrono::high_resolution_clock::now();
+        sleep(5);
 
-
+        auto durationAsyc = std::chrono::duration_cast<std::chrono::microseconds>(t4 - t3).count();
+        std::cout << "Parallel async read time with numThread " << numThreads << "   " << float(durationAsyc / 5000000.)
+                  << std::endl;
+        sleep(1);
+    }
     return 0;
 }
